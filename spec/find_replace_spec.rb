911425e72a3eb73b7973a79145c57e6d92b745c0
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-06 17:24:46 -0700

    add test for third spec, expect to fail.

--- spec/find_replace_spec.rb
+++ spec/find_replace_spec.rb
@@ -10,4 +10,8 @@ describe("Find_replace_word#find_replace") do
     findword = Find_replace_word.new("Hello world", "world", "Universe")
     expect(findword.word_replacer()).to(eq("Hello Universe"))
   end
+  it("finds and replaces even partial words") do
+    findword = Find_replace_word.new("I am walking my dog to the Cathedral", "dog", "cat")
+    expect(findword.word_replacer()).to(eq("i"))
+  end
 end
