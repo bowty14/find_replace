15f797ff75bdb6c8fd9cfab1ac4ff5760fe5b104
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-06 17:21:28 -0700

    add the minimum code to make the test pass.

--- spec/find_replace_spec.rb
+++ spec/find_replace_spec.rb
@@ -8,5 +8,6 @@ describe("Find_replace_word#find_replace") do
   end
   it('finds and replaces the inputted word') do
     findword = Find_replace_word.new("Hello world", "world", "Universe")
-    expect(findword.word_replacer()).to(eq("Hello"))
+    expect(findword.word_replacer()).to(eq("Hello Universe"))
   end
+end
